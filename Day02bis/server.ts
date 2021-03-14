24ccfb784774ebc7d8c3dda4f25bcfa1a7eb1ce1
xTiiX <56162267+xTiiX@users.noreply.github.com>
2021-03-14 12:54:02 +0100

    Delete Day02bis directory

--- Day02bis/server.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const server = express();
-
-server.get('/hello', (req : Request, res: any) => {
-  res.send('world');
-});
-
-server.listen(8080);
